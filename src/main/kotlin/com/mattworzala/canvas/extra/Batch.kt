60abaae2080f74c564558ec410434b2a03bb0d78
Matt Worzala <mattheworzala@gmail.com>
2021-05-31 12:35:57 -0400

    add indices batch & cleanup some docs

--- src/main/kotlin/com/mattworzala/canvas/extra/Batch.kt
+++ src/main/kotlin/com/mattworzala/canvas/extra/Batch.kt
@@ -9,8 +9,7 @@ import net.minestom.server.utils.validate.Check
  *
  * @param cols The columns to set, starting at 0
  * @param handler The slot DSL handler
- *
- * @return A list of ints from every slot set.
+ * @return A list of the affected slots
  */
 fun RenderContext.col(vararg cols: Int, handler: SlotFunc = {}): List<Int> = apply(cols.flatMap(::col), handler)
 
@@ -18,11 +17,9 @@ fun RenderContext.col(vararg cols: Int, handler: SlotFunc = {}): List<Int> = app
  * Sets a list of columns (up and down) on a UI.
  *
  * @param cols The columns to set as a range
- * (1..5 sets all colums 1, 2, 3, 4, and 5)
- *
+ *             (1..5 sets all colums 1, 2, 3, 4, and 5)
  * @param handler The slot DSL handler
- *
- * @return A list of ints from every slot set.
+ * @return A list of the affected slots
  */
 fun RenderContext.col(cols: IntRange, handler: SlotFunc = {}): List<Int> = apply(cols.flatMap(::col), handler)
 
@@ -31,29 +28,42 @@ fun RenderContext.col(cols: IntRange, handler: SlotFunc = {}): List<Int> = apply
  *
  * @param rows The rows to set, starting at 0
  * @param handler The slot DSL handler
- *
- * @return A list of ints from every slot set.
+ * @return A list of the affected slots
  */
 fun RenderContext.row(vararg rows: Int, handler: SlotFunc = {}): List<Int> = apply(rows.flatMap(::row), handler)
 
 /**
  * Sets a list of rows (left and right) on a UI.
  *
- * @param rows The columns to set as a range
- * (1..5 sets all rows 1, 2, 3, 4, and 5)
- *
+ * @param rows The rows to set as a range
+ *             (1..5 sets all rows 1, 2, 3, 4, and 5)
  * @param handler The slot DSL handler
- *
- * @return A list of ints from every slot set.
+ * @return A list of the affected slots
  */
 fun RenderContext.row(rows: IntRange, handler: SlotFunc = {}): List<Int> = apply(rows.flatMap(::row), handler)
 
+/**
+ * Sets a list of indices on a UI.
+ *
+ * @param indices The indices to set.
+ * @param handler The slot DSL handler
+ * @return A list of the affected slots
+ */
+fun RenderContext.indices(vararg indices: Int, handler: SlotFunc = {}): List<Int> = apply(indices.toList(), handler)
+
+/**
+ * Sets a list of indices on a UI.
+ *
+ * @param indices The indices to set.
+ * @param handler The slot DSL handler
+ * @return A list of the affected slots
+ */
+fun RenderContext.indices(indices: IntRange, handler: SlotFunc = {}): List<Int> = apply(indices.toList(), handler)
 
 /**
  * Sets a column (up and down) on a UI.
  *
  * @param col The column to set
- *
  * @return A list of slot indexes that were set.
  */
 internal fun RenderContext.col(col: Int): List<Int> {
@@ -65,7 +75,6 @@ internal fun RenderContext.col(col: Int): List<Int> {
  * Sets a column (left and right) on a UI.
  *
  * @param row The row to set
- *
  * @return A list of slot indexes that were set.
  */
 internal fun RenderContext.row(row: Int): List<Int> {
