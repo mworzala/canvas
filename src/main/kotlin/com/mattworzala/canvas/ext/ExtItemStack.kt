a61710f9e20191aa6266be2c227ca76e58c6044c
Tristan <LeoDog896@hotmail.com>
2022-05-27 17:16:10 -0400

    Bump Minestom

--- src/main/kotlin/com/mattworzala/canvas/ext/ExtItemStack.kt
+++ src/main/kotlin/com/mattworzala/canvas/ext/ExtItemStack.kt
@@ -1,7 +1,6 @@
 package com.mattworzala.canvas.ext
 
 import net.kyori.adventure.text.Component
-import net.minestom.server.item.ItemStackBuilder
 
 //var ItemStackBuilder.displayNameString: String
 //    get() = throw IllegalAccessException("Cannot read item name from builder.");
